1b4a598a949f8bc245ee585b4c1096c4c3e42667
hyungjin kim <hjux07@gmail.com>
2023-09-13 15:21:41 +0900

    [✨feat] 게임세팅 로직 함수선언, drag & drop 이벤트 적용 - 게임세팅 로직 setGame함수로 선언 - container에 dragstart, dragover, drop 이벤트 추가( dragover 기본 이벤트 방지 적용 )

--- ts/main.ts
+++ ts/main.ts
@@ -1,3 +1,5 @@
+import { Value, Dragged } from "./types";
+
 // Utils
 const $ = <T extends HTMLElement>(node: string): T => document.querySelector(node) as T;
 const $$ = <T extends NodeListOf<HTMLElement>>(node: string): T => document.querySelectorAll(node) as T;
@@ -8,27 +10,29 @@ const $container = $<HTMLUListElement>(".image-container"),
       $gameText = $<HTMLParagraphElement>('.game-text'),
       $playTime = $<HTMLParagraphElement>('.play-time');
 
-// DOMVariable
-const count: number = 16;
+const value: Value = {
+  positionX: 0, 
+  positionY: 0,
+  tilesLen: 16,
+} 
+let { positionX, positionY, tilesLen } = value;
 
-// Position value
-let positionX: number = 0, 
-    positionY: number = 0,
-
-    // createImageTiles함수 반환 배열을 담을 배열
-    tiles: HTMLLIElement[] = [];
+// createImageTiles함수 반환 배열을 담을 배열
+let tiles: HTMLLIElement[] = [];
 
 
+/* Functions */
 const createImageTiles = (): HTMLLIElement[] => {
 
   // 요소를 받을 빈 배열
   const temArr = [];
 
   // 요소 생성
-  for(let i = 0; i < count; i++){
+  for(let i = 0; i < tilesLen; i++){
     const $li = document.createElement('li');
 
     $li.dataset.index = i + "";
+    $li.draggable = true;
     $li.classList.add(`list${i}`);
     $li.style.backgroundPositionX = `-${positionX}00px`;  
 
@@ -38,6 +42,7 @@ const createImageTiles = (): HTMLLIElement[] => {
     if( i >= 4 && i <= 8 ) positionY = 100;
     if( i >= 8 && i <= 13 ) positionY = 200;
     if( i >= 12 && i <= 17 ) positionY = 300;
+
     $li.style.backgroundPositionY = `-${positionY}px`;      
 
     temArr.push($li);
@@ -57,14 +62,40 @@ const shuffle = <T extends HTMLLIElement[]>( arr: T ): T =>{
 
     index--;
   }
-
   return arr;
 }
 
-tiles = createImageTiles();
+const setGame = () =>{
+  tiles = createImageTiles();
 
-// suffle함수로 순서가 섞인 배열 요소들을 $container에 추가
-shuffle(tiles).forEach((tile: HTMLElement) => {
-  $container.appendChild(tile);
+  tiles.forEach((tile: HTMLElement) => {
+    $container.appendChild(tile);
+  })
+
+  // 일정 시간 뒤 기존에 추가된 요소들 제거 후 suffle함수로 순서가 섞인 배열 요소들을 추가
+  setTimeout(() => {
+    $container.insertAdjacentHTML("beforeend","");
+  
+    
+    shuffle(tiles).forEach((tile: HTMLElement) => {
+    $container.appendChild(tile);
+  })
+  }, 3000);
+}
+
+
+/* Events */
+$container.addEventListener("dragstart",(e: DragEvent) =>{
+  console.log(e);
+})
+$container.addEventListener("dragover",(e: DragEvent) =>{
+  // Element 위에 over된 상태에서 놓으면 drop이벤트가 발생되지 않으므로 기본 이벤트 방지해 발생하지 않게 설정
+  e.preventDefault();
+  console.log('over');
+})
+$container.addEventListener("drop",(e: DragEvent) =>{
+  console.log('dropped');
 })
 
+
+setGame();
---
+++ ts/types.d.ts
@@ -0,0 +1,11 @@
+interface Value {
+  [key: string]: number;
+}
+
+interface Dragged {
+  el: any,
+  class: any,
+  index: any,
+}
+
+export { Value, Dragged };
